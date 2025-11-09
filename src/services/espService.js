29f7490702e4a8f8a0fc863c547d943b777db30d
Paulo77Dev <paulomadson16@gmail.com>
2025-11-09 16:01:17 -0500

    teste final de servidor

--- src/services/espService.js
+++ src/services/espService.js
@@ -1,5 +1,5 @@
 // src/services/espService.js - VERSÃO CORRIGIDA
-const ESP32_IP = "http://192.168.1.17"; // SEU IP DO ESP32
+const ESP32_IP = "http://192.168.1.11"; // SEU IP DO ESP32.17(en mi casa)
 const API_URL = ESP32_IP;
 
 /**
